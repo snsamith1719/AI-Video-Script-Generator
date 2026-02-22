09e2da45110f09a9f54e53b64881bfecda369b82
snsamith1719 <samithrakshan@gmail.com>
2026-02-22 13:28:12 +0530

    frontend domian added in cors

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://ai-video-script-generator-delta.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
